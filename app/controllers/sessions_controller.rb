b9f93c8ab7051371d9d71f0badef30c9b51367f2
Hillevi Eklöw <hillevieklow@gmail.com>
2018-08-13 23:41:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def create
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      render 'welcome/home'
+    else
+      # normal login 
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
